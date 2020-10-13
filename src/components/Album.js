65d56319eedd3b215bdd3bb8e925b28c1777a510
Kacper Janas <kacper.janas@uczen.zsk.poznan.pl>
2020-10-13 13:20:25 +0200

    album template

--- src/components/Album.js
+++ src/components/Album.js
@@ -4,7 +4,27 @@ export class Album extends Component {
     render() {
         return (
             <div>
-                
+                    <div className="album">
+                        <div className="row">
+                            <div className="column">
+                                <img src="./bin/img/o1.jpg" alt=""/>
+                                <img src="./bin/img/o2.jpg" alt=""/>
+                                <img src="./bin/img/o3.jpg" alt=""/>
+                            </div>
+
+                            <div className="column">
+                                <img src="./bin/img/o4.jpg" alt=""/>
+                                <img src="./bin/img/o5.jpg" alt=""/>
+                                <img src="./bin/img/o6.jpg" alt=""/>
+                            </div>
+
+                            <div className="column">
+                                <img src="./bin/img/o7.jpg" alt=""/>
+                                <img src="./bin/img/o8.jpg" alt=""/>
+                                <img src="./bin/img/o9.jpg" alt=""/>
+                            </div>
+                        </div>
+                    </div>
             </div>
         )
     }
